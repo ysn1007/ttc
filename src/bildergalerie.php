7f29ef3036eab5bb440c46834d3e14d5a1d8045b
ysn1007 <yasin.benammar@yahoo.de>
2024-03-31 00:08:14 +0100

    adjustments galery

--- src/bildergalerie.php
+++ src/bildergalerie.php
@@ -2,7 +2,6 @@
 require_once 'admin/dbh.inc.php';
 include('./includes/header.php');
 
-//var_dump($_GET['dekade']);
 $dekade = $_GET['dekade'];
 
 $content = '';
@@ -11,27 +10,25 @@ $content .= '
 <div class="site-wrap col-xs-12 col-sm-12 col-md-12">
     <div class="content-wrap new-player-wrap col-xs-12 col-md-12">
         <div class="row">
-            <div class="col-xs-12 col-md-12 gal-img-group"> 
+            <div class="col-xs-12 col-md-12 gal-img-group "> 
                 <div class="row">';
                 $res = getDekadeImages($con, $dekade);
-                //var_dump($res);
                 while($row = mysqli_fetch_assoc($res)){
-                    //var_dump($dekade);exit;
                     if($dekade === $row['dekade']) {
-                        //var_dump($row);
                         $content .= '
-                        <div class="col-xs-6 col-md-4 gal-img-item">
+                        <div class="col-xs-6 col-md-4 gal-img-item" id="gal-img-item">
                             
-                            <div class="col-xs-12 img-item-body">
+                            <div class="col-xs-12 img-item-body" id="img-item-body">
                                 <div class="img-header">
-                                    <h3>'. $row['title'] .'</h3>
+                                    <!--h4>'. $row['title'] .'</h4-->
                                 </div>
                                 <div class="img-item">
-                                    <img src="'. substr($row['imagePath'], 3 ) .'" class="img" >
+                                    <img src="'. substr($row['imagePath'], 3 ) .'" title="'. $row['title'] .'" class="img" >
                                 </div>
                                 <div class="img-data">
-                                    <div>'. $row['descript'] .'</div>
-                                    <div>'. $row['imageYear'] .'</div>
+                                    <h4>'. $row['title'] .'</h4>
+                                    <!--div>'. $row['descript'] .'</div-->
+                                    <!--div>'. $row['imageYear'] .'</div-->
                                 </div>
                             </div>
                             
