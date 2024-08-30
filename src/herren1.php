56f0e09d448a89672040d1d4f0146316fe94da73
ysn1007 <yasin.benammar@yahoo.de>
2024-08-31 00:44:40 +0200

    adjusted team structure and styles

--- src/herren1.php
+++ src/herren1.php
@@ -3,7 +3,7 @@
     <div class="content-wrap col-xs-12 col-md-12">
         <div class="galery-header">
             <img src="img/tt-icon.svg" alt="">
-            <h2>1. Herren</h2>
+            <h2>1. Herren - Verbandsliga</h2>
         </div>
 
         <section class="team-section col-xs-12">
@@ -13,13 +13,76 @@
 
             <div class="team-data col-xs-12 col-md-6">
                 <div class="team-line-up col-xs-12 col-md-6">
+                    <div class="tab-header">Verbandsliga Nord</div>
                     <ul class="team-group">
-                        <li class="player"><span class="position">1.</span><img src="img/img.jpg" class="img" width="30px" alt="Spieler-bild"> Max Busberg <span class="ttr-points">2000</span></li>
-                        <li class="player"><span class="position">2.</span><img src="img/img.jpg" class="img" width="30px" alt="Spieler-bild"> Emil Stiller <span class="ttr-points">2000</span></li>
-                        <li class="player"><span class="position">3.</span><img src="img/img.jpg" class="img" width="30px" alt="Spieler-bild"> Daniel Bitka <span class="ttr-points">2000</span></li>
-                        <li class="player"><span class="position">4.</span><img src="img/img.jpg" class="img" width="30px" alt="Spieler-bild"> Jose Montero <span class="ttr-points">2000</span></li>
+                        <li class="player-header">
+                            <div class="player-data-header"><span class="position">Pos.</span> Name</div>
+                            <div class="player-points-header"><span class="ttr-points">Punkte</span></div>   
+                        </li>
+
+
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">1.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Max Busberg</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">2.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Emil Stiller</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">3.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Daniel Bitka</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">4.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Jose Buiza Montero</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
                         <li role="separator" class="divider"></li>
-                        <li class="player"><span class="position">5.</span><img src="img/img.jpg" class="img" width="30px" alt="Spieler-bild"> Niko Mattig <span class="ttr-points">2000</span></li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">4.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Niko Mattig</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">5.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Yasin Ben Ammar</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+
                     </ul>
                 </div>
                 <div class="table-line-up col-xs-12 col-md-6">
--- src/herren4.php
+++ src/herren4.php
@@ -5,6 +5,93 @@
             <img src="img/tt-icon.svg" alt="">
             <h2>4. Herren</h2>
         </div>
+
+        <section class="team-section col-xs-12">
+            <div class="team-img col-xs-12 col-md-6">
+                <img src="img/team.jpg" width="100%" alt="Team bild">
+            </div>
+
+            <div class="team-data col-xs-12 col-md-6">
+                <div class="team-line-up col-xs-12 col-md-6">
+                    <div class="tab-header">2. Bezirksliga B</div>
+                    <ul class="team-group">
+
+                        <li class="player-header">
+                            <div class="player-data-header"><span class="position">Pos.</span> Name</div>
+                            <div class="player-points-header"><span class="ttr-points">Punkte</span></div>   
+                        </li>
+
+
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">1.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Peter Höfer</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">2.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Mario Richardt</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">3.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Amir Aboud</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">4.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Andreas Müller</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="divider"></li>
+                        
+
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">5.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Kim Chriatian Harder</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                        
+                        <li class="player-data">
+                            <div class="position">
+                                <div class="pos-nr">6.</div>
+                            </div>
+                            <div class="player">
+                                <div class="player-name">Christopher Böttcher</div>
+                                <div class="player-points"><span class="ttr-points">2000</span></div>   
+                            </div>
+                        </li>
+                       
+                    </ul>
+                </div>
+                <div class="table-line-up col-xs-12 col-md-6">
+                    Hier kommt die Tabelle für die liga.
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 <?php include('./includes/footer.php');?>
