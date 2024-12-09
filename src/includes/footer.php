216db0009edaba2ea4bbc6b47e3b6c47f2c77f77
ysn1007 <yasin.benammar@yahoo.de>
2024-12-09 22:24:50 +0100

    responsive adjustments and deleted not needed files

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -4,13 +4,13 @@
         <footer class="col-xs-12 col-md-12 footer">
             <div class="row">
                 <section class="col-xs-12 col-md-12 footer-section">
-                    <div class="col-xs-6 col-md-3 club">
+                    <div class="col-xs-6 col-md-3 footer-section-item club">
                         <h4>TTC RAMSHARDE</h4>
                         <p>Andreas Müller</p>
                         <p>Telefon: 0152/53917291</p>
                     </div>
 
-                    <div class="col-xs-6 col-md-3 training">
+                    <div class="col-xs-6 col-md-3 footer-section-item training">
                         <h4>Hallenzeiten</h4>
                         <p>Mo: Nach Absprache</p>
                         <p>Di: 17:00 - 18:00 Schüler / Jugend </p>
@@ -19,39 +19,34 @@
                         <p>Fr: 18:00 - 22:00 Herren</p>
                     </div>
                     
-                    <div class="col-xs-6 col-md-3 halle">
-                        <h4>Halle</h4>
+                    <div class="col-xs-6 col-md-3 footer-section-item halle">
+                        <h4>Anfahrt</h4>
                         <p>Petrihalle Halle</p>
                         <p>Apenrader Str. 164</p>
                         <p>24939 Flansburg</p>
                     </div>
                     
-                    
-                    <div class="col-xs-6 col-md-3 social">
+                    <div class="col-xs-6 col-md-3 footer-section-item social">
                         <h4>Social Media</h4>
                         <p>Facebook</p>
                         <p>Instagramm</p>
                     </div>
-
-
                 </section>
 
                 <section class="col-xs-12 col-md-12 footer-links">
                     <div class="row">
-                        <div class="col-xs-6 col-md-9 link-wrap">
+                        <div class="col-xs-12 col-md-9 link-wrap">
                             <div class="link-item"><a href="kontakt.php" class="link">Kontakt</a></div>
                             <div class="link-item"><a href="impressum.php" class="link">Impressum</a></div>
-                            
                         </div>
-                        <div class="col-xs-6 col-md-3 copyright">
+                        <div class="col-xs-12 col-md-3 copyright">
                             <div class="link-item">
-                                <span>TTC RAMSHARDE © 2024 - 2025</span>
+                                <span>TTC RAMSHARDE © 2024-2025</span>
                             </div>          
                         </div>
                     </div>
                 </section>
 
-                
             </div>
         </footer>
     </body>
