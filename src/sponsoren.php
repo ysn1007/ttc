caef4a87b900e1cb7877dba814c7796e51edf51a
ysn1007 <yasin.benammar@yahoo.de>
2026-09-07 17:50:02 +0200

    refactor(fe) : Sponsoren-item Struktur und Abstände angepasst

--- src/sponsoren.php
+++ src/sponsoren.php
@@ -11,18 +11,19 @@ include('./includes/header.php');
                 <img src="img/tt-icon.svg" alt="">
                 <h1>Sponsoren</h1>
             </div>
-
-            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
-                <div class=" sponsor-item">
-                    <div class="row ">
-                        <div class="sponsor">
-                            <div class="row">
-                                <div class="spon-image spon-bg-img">
-                                    <img src="img/sponsoren/sw-logo.png" width="40%" alt="sponsor logo" loading="lazy">
-                                </div>
-                                <div class="sponsor-info">
-                                    <h4>Stadtwerke Flensburg GmbH</h4>
-                                    <p>Die Flensburger Stadtwerke steht uns seit Jahrzeiten zur Seite und wir sind froh darüber so einen großartigen Partner aus unserer Region zu haben. Wir freuen uns weiterhin auf eine enge Zusammenarbeit.</p>
+            <div class="spon-group">
+                <div class="row">
+                    <div class="col-4 sponsor-item">
+                        <div class="row ">
+                            <div class="sponsor">
+                                <div class="row">
+                                    <div class="spon-image spon-bg-img">
+                                        <img src="img/sponsoren/sw-logo.png" width="40%" alt="sponsor logo" loading="lazy">
+                                    </div>
+                                    <div class="sponsor-info">
+                                        <h4>Stadtwerke Flensburg GmbH</h4>
+                                        <p>Die Flensburger Stadtwerke steht uns seit Jahrzeiten zur Seite und wir sind froh darüber so einen großartigen Partner aus unserer Region zu haben. Wir freuen uns weiterhin auf eine enge Zusammenarbeit.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
